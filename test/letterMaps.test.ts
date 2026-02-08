aa6948e1d11d968da046d5353b545f6ff961d3cb
Jared Kuhn <jared.m.kuhn@gmail.com>
2026-02-07 17:41:42 -0800

    Updating Unit tests

--- test/letterMaps.test.ts
+++ test/letterMaps.test.ts
@@ -1,15 +1,29 @@
-import { LETTER_MAPS } from '../src/common/letterMaps';
+import { BASE_MAPS, type CapitalLetters } from '../src/common/letterMaps';
+import { generateLetter } from '../src/common/affineHelpers';
 
-describe('LETTER_MAPS Integrity', () => {
+describe('BASE_MAPS Existence', () => {
     test('contains at least 3 AffineTransform2D entries in total', () => {
         let totalTransforms = 0;
 
-        Object.values(LETTER_MAPS).forEach((transforms) => {
+        Object.values(BASE_MAPS).forEach((transforms) => {
             if (transforms) {
                 totalTransforms += transforms.length;
             }
         });
 
         expect(totalTransforms).toBeGreaterThanOrEqual(3);
+    });
+    test('BASE_MAPS produce valid affine transforms', () => {
+        Object.keys(BASE_MAPS).forEach((k) => {
+            const key = k as CapitalLetters;
+            const map = BASE_MAPS[key];
+            if (!map) return;
+            expect(() => {
+                generateLetter(key);
+            }).not.toThrow();
+            const affine = generateLetter(key);
+            expect(affine).toBeDefined();
+            expect(affine.length).toEqual(map.length);
+        });
     });
 });
